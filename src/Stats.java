16fa15fccb295e47dd2ed86bf470260fc9126855
LukeBMorrow <lukebmorrow@gmail.com>
2018-10-05 23:29:39 -0500

    added quicksort driver, quicksort, and partition. Aswell as some comments

--- src/Stats.java
+++ src/Stats.java
@@ -32,7 +32,9 @@ public class Stats {
                 Math.sqrt(Math.pow(standardDeviation(left),2) + Math.pow(standardDeviation(right),2));
     }
 
-    /////////my code                        ------------------
+
+
+
 
 
     //      SORTING WORK
@@ -62,7 +64,6 @@ public class Stats {
 
 
     //      RECURSIVE WORK
-
     //A RECURSIVE MERGE SORT ALGORITHM
     private static void mergeSort(int[] home, int start, int end, int[] temp)
     {
@@ -112,21 +113,91 @@ public class Stats {
 
 
     //A RECURSIVE QUICK SORT ALGORITHM
+    /*A driver method to ensure intuitive use for the user
+        >home is the main array
+     */
+    private void quicksort(int[] home)
+    {quicksort(home,0,home.length);}
 
-    private void medianOf3(int[] home) {
+    /*The main body of the quicksort algorithm, this method will
+        recursively partition an ever decreasing(in size) part of the array
+        to approximately sorted halves.
 
-        int middle = home.length / 2;
-        int end = home.length-1;
+        -uses partition to approximately sort the halves
+        -uses medianOf3 to find a pivot that is not a worst case scenario.
 
-            if(home[0]<=home[middle] && home[middle]<home[end] ||
-                    home[end]<=home[middle] && home[middle]<home[0])
-                swap(home,0,middle);    //if middle is the median value
-            else if(home[middle]<=home[end] && home[end]<home[0] ||
-                    home[0]<=home[end] && home[middle]<home[end])
-                swap(home,0,end);       //if end is the median value
+         >home is the main array
+         >start is the start of the part being sorted
+         >end is the end of the part being sorted
+     */
+    private void quicksort(int[] home, int start, int end)
+    {
+        if(end-start>1)//run if there are at least two items to compare
+        {
+            medianOf3(home,start,end);//choose a non-terrible pivot
+            int middle=partition(home,start,end);//approx. sort to get the pivot in the middle
+
+            quicksort(home,start,middle);//rec. sort the first half
+            quicksort(home,middle,end);//rec. sort the second half
+        }//END IF
+    }//END QUICKSORT
+
+    /*This method will approximately sort the two halves
+         of the part of the array given around an assumed pivot in the
+         first position (should have been placed there by mediaOf3)
+
+         -uses swap for a simple swapping of two variables
+
+         >home is the main array
+         >start is the start of the part being approx. sorted
+         >end is the end of the part being approx. sorted
+     */
+    private int partition(int[] home, int start , int end)
+    {
+        int bigStart=start+1;//+1 because the pivot is in the first position
+        int curr=start+1;//+1 because the pivot is in the first position
+        while(curr<end)
+        {
+            if(home[curr]<home[start])//current item is smaller than pivot
+            {
+                swap(home, bigStart, curr);
+                bigStart++;//make the small side bigger
+            }
+            curr++;
+        }
+        swap(home,start,bigStart-1);//move pivot to the middle
+        return bigStart-1;
+    }
+
+    /*This method will grab the first, middle, and last item of the
+         array and compare to find the /median of 3/
+
+         -uses swap for a simple swapping of two variables
+
+         >home is the main array
+         >start is the start of the part being viewed
+         >end is the end of the part being viewed
+     */
+    private void medianOf3(int[] home, int start , int end)
+    {
+
+        int middle = start+(end-start)/ 2;
+
+            if(home[start]<=home[middle] && home[middle]<home[end-1] ||
+                    home[end-1]<=home[middle] && home[middle]<home[start])
+                swap(home,start,middle);    //if middle is the median value
+            else if(home[middle]<=home[end-1] && home[end]<home[start] ||
+                    home[start]<=home[end-1] && home[middle]<home[end-1])
+                swap(home,start,end);       //if end is the median value
             //base case is that 0 is the median value
     }
 
+    /*A simple swapping algorithm
+
+        >home is the array being manipulated
+        >a is the first item being swapped
+        >b is the second item being swapped
+     */
     private static void swap(int[] home, int a, int b)
     {
         int temp;
