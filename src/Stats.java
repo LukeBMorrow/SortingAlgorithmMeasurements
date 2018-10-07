5a4637b13c4fc49782b44a12d1fdb9460a05e8bf
LukeBMorrow <lukebmorrow@gmail.com>
2018-10-06 21:28:53 -0500

    Added hybridQuickSort driver, hybridQuickSort, isSorted, and fillArray, aswell as changed some general/arbitrary code in the whole class to better match the requirements.

--- src/Stats.java
+++ src/Stats.java
@@ -1,6 +1,9 @@
 import java.lang.Math;
 
 public class Stats {
+
+    private static final int BREAKPOINT = 50;
+
     /*
      * Creates a mean based from a passed array
      */
@@ -39,24 +42,30 @@ public class Stats {
 
     //      SORTING WORK
     //A NON- RECURSIVE INSERTION SORT ALGORITHM
-    private static void insertionSort(int[] home)
+    public static void insertionSort(int[] nums)
+    {
+        insertionSort(nums,0,nums.length-1);
+    }
+
+    private static void insertionSort(int[] nums, int start, int end)
     {
         int itemValue;
         int sortingIndex;
 
-        for(int i=1; i<home.length; i++)
+        for(int i=start; i<end; i++)
         {
-            itemValue = home[i];//the value we are currently looking to sort
+            itemValue = nums[i];//the value we are currently looking to sort
             sortingIndex = i-1;//our search index for the sorted portion
 
-            //while index is in the array and current item is greater than our value keep looking
-            while(sortingIndex >= 0 && home[sortingIndex] > itemValue)
+            //while index is in the part of the array we are sorting and
+            //current item is greater than our value keep looking.
+            while(sortingIndex >= start && nums[sortingIndex] > itemValue)
             {
-               home[sortingIndex+1] = home[sortingIndex];//moves items to make way for the new element
+               nums[sortingIndex+1] = nums[sortingIndex];//moves items to make way for the new element
                 sortingIndex--;
             }//END WHILE
 
-           home[sortingIndex+1] = itemValue;//add item where while loop stopped
+           nums[sortingIndex+1] = itemValue;//add item where while loop stopped
 
         }//END FOR
     }//END INSERTIONSORT
@@ -65,7 +74,13 @@ public class Stats {
 
     //      RECURSIVE WORK
     //A RECURSIVE MERGE SORT ALGORITHM
-    private static void mergeSort(int[] home, int start, int end, int[] temp)
+    public void mergeSort(int[]nums)
+    {
+        int[] temp = new int[nums.length];
+        mergeSort(nums, 0,nums.length-1,temp);
+    }
+
+    private void mergeSort(int[] nums, int start, int end, int[] temp)
     {
         int mid;
 
@@ -73,15 +88,15 @@ public class Stats {
         {
             mid = start + (end-start)/2;// define the middle of the array
 
-            mergeSort(home, start, mid, temp);//recursively sort the lower half
-            mergeSort(home, mid, end, temp);//recursively sort the upper half
+            mergeSort(nums, start, mid, temp);//recursively sort the lower half
+            mergeSort(nums, mid, end, temp);//recursively sort the upper half
 
-            merge (home, start, mid, end, temp);//merge those two halves into one sorted list
+            merge (nums, start, mid, end, temp);//merge those two halves into one sorted list
 
         }//END IF
     }//END MERGESORT
 
-    private static void merge(int[] home, int start,int mid, int end, int[] temp)
+    private void merge(int[] nums, int start,int mid, int end, int[] temp)
     {
         int currL = start;
         int currR = mid;
@@ -91,20 +106,20 @@ public class Stats {
         {
             /*if('L' is in its proper range, and if ('R' has run out of its range
             or 'a[R]' is bigger than 'a[L]')) */
-            if(currL < mid && (currR >= end || home[currL] < home[currR]))
+            if(currL < mid && (currR >= end || nums[currL] < nums[currR]))
             {
-                temp[currT]= home[currL];//put 'a[L]' in next
+                temp[currT]= nums[currL];//put 'a[L]' in next
                 currL++;
             }//END IF
             else
             {
-                temp[currT] =home[currR];
+                temp[currT] =nums[currR];
                 currR++;
             }//END ELSE
         }//END FOR
 
-        System.arraycopy(temp,0,  home, 0, temp.length);
-        //^deep copy the temp array to the home array
+        System.arraycopy(temp,0,  nums, 0, temp.length);
+        //^deep copy the temp array to the nums array
 
     }//END MERGE
 
@@ -114,10 +129,10 @@ public class Stats {
 
     //A RECURSIVE QUICK SORT ALGORITHM
     /*A driver method to ensure intuitive use for the user
-        >home is the main array
+        >nums is the main array
      */
-    private void quicksort(int[] home)
-    {quicksort(home,0,home.length);}
+    public void quicksort(int[] nums)
+    {quicksort(nums,0,nums.length);}
 
     /*The main body of the quicksort algorithm, this method will
         recursively partition an ever decreasing(in size) part of the array
@@ -126,19 +141,19 @@ public class Stats {
         -uses partition to approximately sort the halves
         -uses medianOf3 to find a pivot that is not a worst case scenario.
 
-         >home is the main array
+         >nums is the main array
          >start is the start of the part being sorted
          >end is the end of the part being sorted
      */
-    private void quicksort(int[] home, int start, int end)
+    private void quicksort(int[] nums, int start, int end)
     {
         if(end-start>1)//run if there are at least two items to compare
         {
-            medianOf3(home,start,end);//choose a non-terrible pivot
-            int middle=partition(home,start,end);//approx. sort to get the pivot in the middle
+            medianOf3(nums,start,end-1);//choose a non-terrible pivot
+            int middle=partition(nums,start,end-1);//approx. sort to get the pivot in the middle
 
-            quicksort(home,start,middle);//rec. sort the first half
-            quicksort(home,middle,end);//rec. sort the second half
+            quicksort(nums,start,middle);//rec. sort the first half
+            quicksort(nums,middle,end-1);//rec. sort the second half
         }//END IF
     }//END QUICKSORT
 
@@ -148,25 +163,25 @@ public class Stats {
 
          -uses swap for a simple swapping of two variables
 
-         >home is the main array
+         >nums is the main array
          >start is the start of the part being approx. sorted
          >end is the end of the part being approx. sorted
      */
-    private int partition(int[] home, int start , int end)
+    private static int partition(int[] nums, int start , int end)
     {
         int bigStart=start+1;//+1 because the pivot is in the first position
         int curr=start+1;//+1 because the pivot is in the first position
-        while(curr<end)
+        while(curr<=end)
         {
-            if(home[curr]<home[start])//current item is smaller than pivot
+            if(nums[curr]<nums[start])//current item is smaller than pivot
             {
-                swap(home, bigStart, curr);
+                swap(nums, bigStart, curr);
                 bigStart++;//make the small side bigger
             }
             curr++;
         }
-        swap(home,start,bigStart-1);//move pivot to the middle
-        return bigStart-1;
+        swap(nums,start,bigStart-1);//move pivot to the middle
+        return bigStart-1;//returns the current position of the pivot
     }
 
     /*This method will grab the first, middle, and last item of the
@@ -174,46 +189,94 @@ public class Stats {
 
          -uses swap for a simple swapping of two variables
 
-         >home is the main array
+         >nums is the main array
          >start is the start of the part being viewed
          >end is the end of the part being viewed
      */
-    private void medianOf3(int[] home, int start , int end)
+    private static void medianOf3(int[] nums, int start , int end)
     {
-
         int middle = start+(end-start)/ 2;
 
-            if(home[start]<=home[middle] && home[middle]<home[end-1] ||
-                    home[end-1]<=home[middle] && home[middle]<home[start])
-                swap(home,start,middle);    //if middle is the median value
-            else if(home[middle]<=home[end-1] && home[end]<home[start] ||
-                    home[start]<=home[end-1] && home[middle]<home[end-1])
-                swap(home,start,end);       //if end is the median value
+            if(nums[start]<=nums[middle] && nums[middle]<nums[end] ||
+                    nums[end]<=nums[middle] && nums[middle]<nums[start])
+                swap(nums,start,middle);    //if middle is the median value
+
+            else if(nums[middle]<=nums[end] && nums[end]<nums[start] ||
+                    nums[start]<=nums[end] && nums[middle]<nums[end])
+                swap(nums,start,end);       //if end is the median value
             //base case is that 0 is the median value
     }
 
     /*A simple swapping algorithm
 
-        >home is the array being manipulated
+        >nums is the array being manipulated
         >a is the first item being swapped
         >b is the second item being swapped
      */
-    private static void swap(int[] home, int a, int b)
+    private static void swap(int[] nums, int a, int b)
     {
         int temp;
-        temp = home[a];
-        home[a] = home[b];
-        home[b] = temp;
+        temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
-    //A HYBRID QUICK SORT ALGORITHM THAT USES A BREAKPOINT.
 
+
+    //A HYBRID QUICK SORT ALGORITHM THAT USES A BREAKPOINT.
+    public static void hybridQuickSort(int[] nums)
+    {
+        hybridQuickSort(nums,0,nums .length-1);
+    }
+
+    private static void hybridQuickSort(int[]nums, int start, int end)
+    {
+        if(end-start>BREAKPOINT)//run if there are at least two items to compare
+        {
+            medianOf3(nums,start,end-1);//choose a non-terrible pivot
+            int middle=partition(nums,start,end-1);//approx. sort to get the pivot in the middle
+
+            hybridQuickSort(nums,start,middle);//rec. sort the first half
+            hybridQuickSort(nums,middle,end-1);//rec. sort the second half
+        }//END IF
+        else
+        {
+            insertionSort(nums, start, end-1);//non-recursive sort the rest of the array
+        }
+    }
 
     //--------------------------------------------------------------------------
 
     //      NON SORTING WORK
     //A METHOD THAT VERIFIES THAT AN ARRAY IS IN SORTED ORDER
+    public static boolean isSorted(int[] nums, String sortType)
+    {
+        int currentSortedIndex=0;
+        while(currentSortedIndex<nums.length && nums[currentSortedIndex]<nums[currentSortedIndex+1])
+        { currentSortedIndex++; }//look for a place where the data is out of order
+
+
+        if(!(nums[currentSortedIndex]<nums[currentSortedIndex+1]))
+        {
+            System.out.println("Sorting type: " +sortType+" :FAILED at position: "+currentSortedIndex);
+            return false;
+        }else//END IF
+        {
+            System.out.println("Sorting type: "+sortType+" :ran SUCCESSFULLY");
+            return true;
+        }//END ELSE
+    }//END ISSORTED
+
 
     //A METHOD TO FILL AN ARRAY WITH VALUES TO SORT
+    public static void fillArray(int[] nums, int n)
+    {
+        for(int currentFillingIndex:nums)//fills the array
+        { nums[currentFillingIndex]=currentFillingIndex; }
+
+
+        for(int swapCount = 0; swapCount<=n;swapCount++)//preforms a random 'n' swaps
+        { swap(nums,(int)((nums.length)*Math.random()),(int)((nums.length)*Math.random()));}
+    }
 
     //A TESTING METHOD THAT ALLOWS YOU TO COMPARE AND TO VERIFY THESE SORTING METHODS USING SOME SIMPLE STATISTICS
 
